3b574208420d20b716449cfc77efc0b99240e7a5
Armanur Rashid <armanurrashid105086@gmail.com>
2025-04-19 03:41:54 +0600

    Scan QR Code

--- lib/views/scan_qr_code.dart
+++ lib/views/scan_qr_code.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class Scan_QR_Code extends StatefulWidget {
   const Scan_QR_Code({super.key});
@@ -8,8 +11,74 @@ class Scan_QR_Code extends StatefulWidget {
 }
 
 class _Scan_QR_CodeState extends State<Scan_QR_Code> {
+  String qrResult = 'Scanned Data will appear here';
+  Future<void> scanQR() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      if (!mounted) return;
+      setState(() {
+        this.qrResult = qrCode.toString();
+      });
+    } on PlatformException {
+      qrResult = 'Fail to read QR Code';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "QR Scanner",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Place the QR Code in the area",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Scanning will be started automatically",
+              style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey),
+            ),
+            SizedBox(
+              height: 150,
+            ),
+            Text('$qrResult',style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey),),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: scanQR,
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(200, 50)),
+              child: Text(
+                "Scan Code",
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
